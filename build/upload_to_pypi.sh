86f472238d3d1f320bb72e2fd28b7a748cb740d9
Aditya Vaja <wolverine.av@gmail.com>
2016-05-16 14:16:49 -0700

    force success git tag

--- build/upload_to_pypi.sh
+++ build/upload_to_pypi.sh
@@ -8,7 +8,7 @@ mv $PYPIRC_FILE ~/.pypirc
 tar -zxvf $GNUPG_TAR -C ~/
 
 echo 'CURR_VERSION=' $CURR_VERSION
-git tag -s $CURR_VERSION -m $CURR_VERSION -u "Big Switch Networks"
+git tag -f -s $CURR_VERSION -m $CURR_VERSION -u "Big Switch Networks"
 
 python setup.py sdist
 
